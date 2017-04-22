7f9120a6b552fc0b56788c57130e0c1fdbe81af2
MiszczProgramowania <budzyn.grzegorz.it@gmail.com>
2017-04-22 14:02:27 +0200

    Astar Graph works

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 
+// Not best way but i don't have much time for project preperation for old js libraries
+declare var Graph:any;
+// Not best way ends
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +11,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app works!';
+  constructor(){
+    console.log('teast');
+    var graph = new Graph([
+      [1,1,1,1],
+      [0,1,1,0],
+      [0,0,1,1]
+    ]);
+    console.log(graph);
+  }
 }
